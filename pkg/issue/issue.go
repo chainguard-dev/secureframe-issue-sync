8d183ea020a001c5189b1588cf822f500afe8e36
Thomas Stromberg <t+github@chainguard.dev>
2023-01-12 09:14:10 -0500

    Add Markdown conversion for remediation steps, fix empty label

--- pkg/issue/issue.go
+++ pkg/issue/issue.go
@@ -5,9 +5,11 @@ import (
 	_ "embed"
 	"fmt"
 	"html"
+	"log"
 	"strings"
 	"text/template"
 
+	md "github.com/JohannesKaufmann/html-to-markdown"
 	"github.com/chainguard-dev/secureframe-github-sync/pkg/secureframe"
 )
 
@@ -20,17 +22,57 @@ type IssueForm struct {
 	Labels []string
 }
 
-func FromTest(t secureframe.Test, label string, reportKey string) (IssueForm, error) {
+func assertWork(a secureframe.AssertionResult) string {
+	work := strings.TrimSpace(a.FailMessage)
+	// TODO: Don't hardcode this
+	url := "https://app.secureframe.com/dashboard/incomplete-tests/soc2-beta"
+
+	if work == "" {
+		work = fmt.Sprintf("Upload evidence for %s", a.Data.Type)
+	}
+
+	if a.Resourceable != nil {
+		return fmt.Sprintf("%s: %s", secureframe.ResourceID(*a.Resourceable), work)
+	}
+
+	if strings.HasPrefix(work, "Upload") {
+		work := strings.TrimRight(work, ".")
+		return work + " to " + url
+	}
+
+	if strings.HasPrefix(work, "Select") && strings.HasSuffix(work, "Policy") {
+		return work + " at " + url
+	}
+
+	return work
+}
+
+func makeMarkdown(html string) string {
+	converter := md.NewConverter("", true, nil)
+	markdown, err := converter.ConvertString(html)
+	if err != nil {
+		log.Printf("markdown conversion failed: %v", err)
+		return fmt.Sprintf("markdown conversion failed: %v\nraw html: %s", err, html)
+	}
+	return markdown
+}
+
+func FromTest(t secureframe.Test, additionalLabel string, reportKey string) (IssueForm, error) {
 	reportLabel, _, _ := strings.Cut(reportKey, "_")
+	labels := []string{SyncLabel, reportLabel}
+	if additionalLabel != "" {
+		labels = append(labels, additionalLabel)
+	}
 
 	i := IssueForm{
 		Title:  fmt.Sprintf("%s: %s", t.V2.Key, t.V2.Title),
-		Labels: []string{SyncLabel, label, reportLabel},
+		Labels: labels,
 	}
 
 	tmpl, err := template.New("issue").Funcs(template.FuncMap{
 		"Unescape":   html.UnescapeString,
-		"AssertWork": secureframe.AssertWork,
+		"AssertWork": assertWork,
+		"Markdown":   makeMarkdown,
 	}).Parse(issueTmpl)
 	if err != nil {
 		return i, fmt.Errorf("parse: %v", err)
